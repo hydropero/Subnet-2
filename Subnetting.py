88b80dafcc49022933ccdb241903be86214aceec
hydropero <54414748+hydropero@users.noreply.github.com>
2020-05-19 03:40:53 -0500

    subnetting files

---
+++ Subnetting.py
@@ -0,0 +1,80 @@
+import string
+import sys
+import cidr_dec_conv
+from tkinter import *
+
+
+def test():
+    return cidrcalculator()
+
+def test2():
+    print("Apparently works fine now")
+def startup():
+
+    starter = True
+    while starter:
+        print("Welcome to the all in one IP Addressing/Subnetting Tool")
+        intro = str(input("Would you like to get started? Y / N: "))
+        intro = intro.upper()
+        if intro == 'Y':
+            print("Great let's begin!")
+            optionsmenu()
+        elif intro == 'N':
+            print("Sorry to hear that, maybe next time!")
+            sys.exit()
+        else:
+            print("Invalid option, please try again")
+
+def optionsmenu():
+    print("we're now in the options menu function")
+    starter = True
+    while starter:
+        print("Please select a tool from the options below\n")
+        print("For an in depth subnetting tutorial select --> [1]")
+        print("To perform a CIDR to decimal subnet conversion or vice versa select --> [2]")
+        print("To determine network/subnet boundaries select --> [3]")
+        print("To request subnetting practice questions select --> [4]")
+        switch_answer = input("Please enter your selection here: ")
+
+        switcher = {
+            "1": "test",
+            "2": test,
+            "3": test2,
+            "4": "test4"
+        }
+
+        switcher[switch_answer]()
+
+
+
+
+
+
+
+
+def cidrcalculator():
+
+    starter = True
+    while starter:
+        print("-------------------------------------------------------------------------------------------------------")
+        print("Welcome to the subnet converter")
+        form = int(input("CIDR --> Decimal notation [1] \nDecimal notation --> CIRD [2]: \nReturn to main menu [3]"))
+        if form == 1:
+            cidr = int(input("Please enter CIDR number /"))
+            cidr_dec_conv.cidrconvert(cidr)
+        elif form == 2:
+            decimal = input("Please enter subnet mask in decimal notation:")
+            cidr_dec_conv.dec_convert(decimal)
+        elif form == 3:
+            optionsmenu()
+        else:
+            print("Invalid option, please try again")
+
+
+
+
+
+startup()
+
+
+
---
+++ cidr_dec_conv.py
@@ -0,0 +1,95 @@
+import math
+
+def cidrconvert(cidr):
+    temp = ['0', '0', '0', '0', '0', '0', '0', '0']
+    totalsubsizelist = []
+    stringedsublist = []
+    if 0 <= cidr:
+        maximum = 256
+        total = 0
+        current = 0
+        counter = 0
+        sizeofdiv = 2
+        for i in range(0, cidr):
+            current = maximum / sizeofdiv
+            total += current
+            sizeofdiv *= 2
+        totalsubsizelist.append(math.floor(total))
+
+
+
+
+
+    if cidr >= 8:
+        maximum = 256
+        total = 0
+        current = 0
+        counter = 0
+        sizeofdiv = 2
+        for i in range(8, cidr):
+            current = maximum / sizeofdiv
+            total += current
+            sizeofdiv *= 2
+        totalsubsizelist.append(math.floor(total))
+
+
+    if cidr >= 16:
+        maximum = 256
+        total = 0
+        current = 0
+        counter = 0
+        sizeofdiv = 2
+        for i in range(16, cidr):
+            current = maximum / sizeofdiv
+            total += current
+            sizeofdiv *= 2
+
+        totalsubsizelist.append(math.floor(total))
+
+
+    if cidr >= 24:
+        maximum = 256
+        total = 0
+        current = 0
+        counter = 0
+        sizeofdiv = 2
+        for i in range(24, cidr):
+            current = maximum / sizeofdiv
+            total += current
+            sizeofdiv *= 2
+        totalsubsizelist.append(math.floor(total))
+    for i in totalsubsizelist:
+        i = str(i)
+        stringedsublist.append(i)
+
+    test = ".".join(stringedsublist)
+    print(test)
+    return test
+
+
+def dec_convert(decimal):
+    decimaldict = {
+        0: 0,
+        128: 1,
+        192: 2,
+        224: 3,
+        240: 4,
+        248: 5,
+        252: 6,
+        254: 7,
+        255: 8
+    }
+    decimallist = decimal.split(".")
+    CIDR_total = 0
+    for i in decimallist:
+        i = int(i)
+        CIDR_total += decimaldict.get(i, "Error invalid entry was submitted")
+    print(CIDR_total)
+
+
+
+
+
+
+
+
