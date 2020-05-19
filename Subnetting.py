083af2b93898ba80691fef5004b10f2829430bfe
hydropero <mylesjbulla@gmail.com>
2020-05-19 17:04:15 -0500

    question_begin

--- Subnetting.py
+++ Subnetting.py
@@ -1,19 +1,45 @@
 import string
 import sys
+import random
 import cidr_dec_conv
 from tkinter import *
 
+
+def questionCreationFunc():
+
+    generatedcirdlist= []
+
+
+    for i in range(0, 10):
+        count = 0
+        randCidrGenerator = random.randint(10, 32)
+        x = randCidrGenerator
+        y = cidr_dec_conv.cidrconvert(x)
+        generatedcirdlist.append([x, y])
+
+        count += 1
+    print(generatedcirdlist)
+
+
+
+questionCreationFunc()
 
 def test():
     return cidrcalculator()
 
 def test2():
     print("Apparently works fine now")
+
+
+def test5():
+    print("Starting the test!")
+
+    print("Quick what is /" + str(randCidrGenerator) + "in decimal form")
 def startup():
 
     starter = True
     while starter:
-        print("Welcome to the all in one IP Addressing/Subnetting Tool")
+        print("Welcome to the IP Addressing/Subnetting Tool")
         intro = str(input("Would you like to get started? Y / N: "))
         intro = intro.upper()
         if intro == 'Y':
@@ -29,18 +55,23 @@ def optionsmenu():
     print("we're now in the options menu function")
     starter = True
     while starter:
+
         print("Please select a tool from the options below\n")
-        print("For an in depth subnetting tutorial select --> [1]")
+        print("For an in depth subnetting tutorial select ---------------------------> [1]")
         print("To perform a CIDR to decimal subnet conversion or vice versa select --> [2]")
-        print("To determine network/subnet boundaries select --> [3]")
-        print("To request subnetting practice questions select --> [4]")
+        print("To determine network/subnet boundaries select ------------------------> [3]")
+        print("To request subnetting practice questions select ----------------------> [4]")
+        print("To run the subnetting gauntlet select --------------------------------> [5]")
         switch_answer = input("Please enter your selection here: ")
 
         switcher = {
             "1": "test",
             "2": test,
             "3": test2,
-            "4": "test4"
+            "4": "test4",
+            "5": test5
+
+
         }
 
         switcher[switch_answer]()
